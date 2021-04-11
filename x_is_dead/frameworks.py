e9e7411e3c832ec6330dc56f1c3dc69586f5b030
Michaël Malter <3661747+mmalter@users.noreply.github.com>
2021-04-11 03:39:21 +0200

    Better React detection

--- x_is_dead/frameworks.py
+++ x_is_dead/frameworks.py
@@ -35,7 +35,7 @@ def react_checker(d):
     script = """
     if(
     !!window.React ||
-    !!document.querySelector('.ng-binding, [ng-app], [data-ng-app], [ng-controller], [data-ng-controller], [ng-repeat], [data-ng-repeat]')
+    !!document.querySelector('[data-reactroot], [data-reactid]')
     )
 
     {
